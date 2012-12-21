67d9b55528df6226c4a86fd1ff8f3861a0fbf137
Daniel Del Rio <ddelrio1986@gmail.com>
2012-12-21 10:17:25 -0500

    Add the version information

---
+++ version.php
@@ -0,0 +1,8 @@
+<?php
+
+$module->version = 2012122100;
+$module->requires = 2012120300; // Moodle 2.4
+$module->cron = 0;
+$module->component = 'mod_resume';
+$module->maturity = MATURITY_ALPHA;
+$module->release = '0.1 (Build: 2012122100)';

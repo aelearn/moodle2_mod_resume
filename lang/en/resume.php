97bdba47e760e408b758ab415abcffe28e83700f
Daniel Del Rio <ddelrio1986@gmail.com>
2012-12-21 10:14:03 -0500

    Remove reference to the start module

--- lang/en/resume.php
+++ lang/en/resume.php
@@ -4,5 +4,5 @@ $string['modulename'] = 'Resume';
 $string['modulename_help'] = 'A module for Moodle 2 that will allow a user to "resume" a course by sending them to the last page they visited according to the logs.';
 $string['modulename_link'] = 'mod/resume/view';
 $string['modulenameplural'] = 'Resumes';
-$string['startname'] = 'Title';
+$string['resumename'] = 'Title';
 $string['pluginadministration'] = 'Edit Resume';

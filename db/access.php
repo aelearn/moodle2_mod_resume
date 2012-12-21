9ca5314a619f9976cbc762550096945baecf0a3c
Daniel Del Rio <ddelrio1986@gmail.com>
2012-12-21 10:01:42 -0500

    Define the default capabilities

---
+++ db/access.php
@@ -0,0 +1,15 @@
+<?php
+
+$capabilities = array(
+    
+    // Ability to add a new resume module to the course.
+    'mod/resume:addinstance' => array(
+        'captype' => 'write',
+        'contextlevel' => CONTEXT_COURSE,
+        'archetypes' => array(
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        ),
+        'clonepermissionsfrom' => 'moodle/course:manageactivities'
+    ),
+);

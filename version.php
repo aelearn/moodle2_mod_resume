49f9371e55ec4de00758248b6f75accabb7b3f88
Daniel Del Rio <ddelrio1986@gmail.com>
2012-12-21 10:44:02 -0500

    Update the version

--- version.php
+++ version.php
@@ -1,8 +1,8 @@
 <?php
 
-$module->version = 2012122100;
+$module->version = 2012122101;
 $module->requires = 2012120300; // Moodle 2.4
 $module->cron = 0;
 $module->component = 'mod_resume';
-$module->maturity = MATURITY_ALPHA;
-$module->release = '0.1 (Build: 2012122100)';
+$module->maturity = MATURITY_STABLE;
+$module->release = '1.0 (Build: 2012122101)';
